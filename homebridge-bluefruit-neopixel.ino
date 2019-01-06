925dcfb94a9885883ea81923e4e83dfad8581188
Tara Neier <tara@neier.org>
2019-01-06 12:32:17 -0800

    cleaned up and working for lightbulb on/off

--- homebridge-bluefruit-neopixel.ino
+++ homebridge-bluefruit-neopixel.ino
@@ -17,6 +17,7 @@ int32_t temperatureCharacteristic;
 
 int32_t lightService;
 int32_t lightCharacteristic;
+int32_t hueCharacteristic;
 
 union float_bytes {
   float value;
@@ -66,14 +67,21 @@ void setup(void) {
                                                      GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_NOTIFY,
                                                      sizeof(float), sizeof(float), BLE_DATATYPE_BYTEARRAY);
 
+
+
+//  8e76f000-690e-472e-88c3-051277686a73
   uint8_t lightServiceUUID[] = {0x8E,0x76,0xF0,0x00,0x69,0x0E,0x47,0x2E,0x88,0xC3,0x05,0x12,0x77,0x68,0x6A,0x73};
 //  uint8_t lightServiceUUID[] = {0x1D,0x8A,0x68,0xE0,0xE6,0x8E,0x4F,0xED,0x94,0x3E,0x36,0x90,0x99,0xF5,0xB4,0x99};
   lightService = gatt.addService(lightServiceUUID);
-  uint8_t lightCharacteristicUUID[] = {0x8E,0x76,0xF0,0x00,0x69,0x0E,0x47,0x2E,0x88,0xC3,0x05,0x12,0x77,0x68,0x6A,0x73};
+  uint8_t lightCharacteristicUUID[] = {0x8E,0x76,0xF0,0x01,0x69,0x0E,0x47,0x2E,0x88,0xC3,0x05,0x12,0x77,0x68,0x6A,0x73};
   lightCharacteristic = gatt.addCharacteristic(lightCharacteristicUUID,
                                                      GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_NOTIFY,
                                                      sizeof(bool),sizeof(bool), BLE_DATATYPE_AUTO, "On");
-
+                                                 
+  uint8_t hueCharacteristicUUID[] = {0x8E,0x76,0xF0,0x02,0x69,0x0E,0x47,0x2E,0x88,0xC3,0x05,0x12,0x77,0x68,0x6A,0x73};
+  hueCharacteristic = gatt.addCharacteristic(hueCharacteristicUUID,
+                                                     GATT_CHARS_PROPERTIES_READ | GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_NOTIFY,
+                                                     sizeof(bool),sizeof(bool), BLE_DATATYPE_AUTO, "On");
 
 
                                                      
@@ -83,31 +91,27 @@ void setup(void) {
   ble.setConnectCallback(centralConnect);
   ble.setDisconnectCallback(centralDisconnect);
   Serial.println("Bluetooth on");
-  ble.setBleUartRxCallback(BleUartRX);
+
 
   /* Only one BLE GATT function should be set, it is possible to set it
     multiple times for multiple Chars ID  */
-  ble.setBleGattRxCallback(lightCharacteristic, BleGattRX);
-//  ble.setBleGattRxCallback(charid_number, BleGattRX);
+  ble.setBleGattRxCallback(lightCharacteristic, lightRX);
+  ble.setBleGattRxCallback(hueCharacteristic, lightRX);
+
 }
 
-void BleUartRX(char data[], uint16_t len)
-{
-  Serial.print( F("[BLE UART RX]" ) );
-  Serial.write(data, len);
-  Serial.println();
-}
 
-void BleGattRX(int32_t chars_id, uint8_t data[], uint16_t len)
+void lightRX(int32_t chars_id, uint8_t data[], uint16_t len)
 {
   Serial.print( F("[BLE GATT RX] (" ) );
   Serial.print(chars_id);
+  
   Serial.print(") ");
-
+  for (int i = 0; i < len; i++) { Serial.print(i); Serial.print(":"); Serial.print(data[i]);Serial.print("  ");}
   if (chars_id == lightCharacteristic)
   {
 
-    for (int i = 0; i < len; i++) { Serial.print(i); Serial.print(":"); Serial.print(data[i]);Serial.print("  ");}
+//    for (int i = 0; i < len; i++) { Serial.print(i); Serial.print(":"); Serial.print(data[i]);Serial.print("  ");}
 
     Serial.print(len);
     
@@ -133,7 +137,7 @@ void setLED(bool on) {
   pixel.show();
   
 }
-/** Send randomized heart rate data continuously **/
+
 void loop(void) {
   ble.update();
 
@@ -149,15 +153,8 @@ void loop(void) {
     gatt.setChar(temperatureCharacteristic, averageCelsius.bytes, sizeof(averageCelsius));
     Serial.print("Update temperature | ");
     Serial.println(averageCelsius.value);
-  } else {
-//    Serial.print("Temperature | ");
-//    Serial.println(averageCelsius.value);
-  }
-//  if (irq_event_available)
-//  {
-    // Registered callbacks for the event will be fired accordingly
-    ble.handleDfuIrq();
-//  }
+  } 
+
   delay(1000);
 }
 
